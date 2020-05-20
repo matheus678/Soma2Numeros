b1b2b589c41905e671eb91202c4028a55985446c
matheus678 <60980352+matheus678@users.noreply.github.com>
2020-05-20 10:20:56 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace soma2Numeros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            Console.Write("Digite um número...:");
+        
+
+          int X = Int32.Parse (Console.ReadLine ()); 
+
+
+
+          Console.WriteLine("Digite outro número...:");
+
+          int Y = Int32.Parse (Console.ReadLine ());
+
+
+
+          int soma = X + Y; 
+
+          Console.WriteLine ("Resultado da soma:" + soma);
+
+          
+
+          Console.ReadKey ();
+        
+        
+        
+        }
+    }
+}
